d095d674010505ca1ee68d3375c369e4108fdd52
idist-hn <minhtoan96.it@gmail.com>
2024-08-08 01:14:34 +0700

    update config

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #go get github.com/pilu/fresh
 sed -i -e "13 s/version: .*/version: $(date '+%Y.%m.%d.%H.%M')/" ./configs/development/app.yaml
 sed -i -e "14 s/branch: .*/branch: $(git branch --show-current)[$(git rev-parse HEAD)]/" ./configs/development/app.yaml
