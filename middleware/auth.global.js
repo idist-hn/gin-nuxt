346d5a0b5286f98e54535acdf879539ac4dd35cc
idist-hn <minhtoan96.it@gmail.com>
2024-08-07 17:48:02 +0700

    fix authentication

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -1,6 +1,4 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-    console.log({access_token: useCookie("access_token").value, "auth": to.name })
-
     if (!useCookie("access_token").value && to.name && !to.name.startsWith("auth")) {
         console.log({access_token: useCookie("access_token").value, "auth": to.name })
         return navigateTo({ name: "auth-login"});
